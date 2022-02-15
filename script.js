54c21103d84b6becdda362cecdbd825e5ad2c340
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-15 14:38:20 -0500

    Requisitos 4, 5 e 6 - Quadro de pixel, Elementos do quadro e Cor Preta selecionada

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
- 
+window.onload = function() {
+
+}
+
+
+
+const pixelBoard = document.getElementById('pixel-board');
+const pixelCreate = [];
+
+console.log(pixelBoard);
+
+let linePixels = 5;
+let pixelDimension = linePixels * linePixels;
+
+pixelBoard.style.width = (40 * linePixels) + 10 + 'px';
+
+for (let index = 0; index < pixelDimension; index += 1) {
+ pixelCreate[index] = document.createElement('div')
+ pixelBoard.appendChild(pixelCreate[index]).className = 'pixel';
+}
