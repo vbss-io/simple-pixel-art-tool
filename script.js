1b55aa84b93921ad8517a5212b644f9fc908d1dd
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-15 17:26:06 -0500

    Requisito 8 - Pintar os pixels + Reestruturação do código pra satisfazer o requisito

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const pixelBoard = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
+const pixelColors = [];
 const pixelCreate = [];
-
-console.log(pixelBoard);
 
 const linePixels = 5;
 const pixelDimension = linePixels * linePixels;
+
+const colorNumber = 4;
+
+for (let index = 0; index < colorNumber; index += 1) {
+  pixelColors[index] = document.createElement('div');
+  colorPalette.appendChild(pixelColors[index]).className = 'color';
+}
+
+pixelColors[0].style.backgroundColor = 'black';
+pixelColors[0].classList.add('selected');
+pixelColors[1].style.backgroundColor = 'blue';
+pixelColors[2].style.backgroundColor = 'green';
+pixelColors[3].style.backgroundColor = 'red';
 
 // Concatenado atomaticamente pelo VSCode - Problema Lint
 pixelBoard.style.width = `${(40 * linePixels) + 10}px`;
@@ -14,25 +27,30 @@ for (let index = 0; index < pixelDimension; index += 1) {
   pixelBoard.appendChild(pixelCreate[index]).className = 'pixel';
 }
 
-const colors = document.querySelectorAll('.color');
-console.log(colors[0].className);
-
-function CleanColorSelected() {
-  for (let index = 0; index < colors.length; index += 1) {
-    if (colors[index].className.includes('selected')) {
-      colors[index].classList.remove('selected');
+function cleanColorSelected() {
+  for (let index = 0; index < pixelColors.length; index += 1) {
+    if (pixelColors[index].className.includes('selected')) {
+      pixelColors[index].classList.remove('selected');
     }
   }
 }
 
 function colorSelect(color) {
   if (!color.target.className.includes('selected')) {
-    console.log('nao tem');
-    CleanColorSelected();
+    cleanColorSelected();
     color.target.classList.add('selected');
   }
 }
 
-for (let index = 0; index < colors.length; index += 1) {
-  colors[index].addEventListener('click', colorSelect);
+for (let index = 0; index < pixelColors.length; index += 1) {
+  pixelColors[index].addEventListener('click', colorSelect);
+}
+
+function changePixelColor(pixel) {
+  let currentColor = document.querySelector('.selected').style.backgroundColor;
+  pixel.target.style.backgroundColor = currentColor;
+}
+
+for (let index = 0; index < pixelCreate.length; index += 1) {
+  pixelCreate[index].addEventListener('click', changePixelColor);
 }
