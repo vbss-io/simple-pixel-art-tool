8e3166db4914885150efa7532a89eff3ee6c2d70
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-16 16:19:55 -0500

    Finalizando Requisitos Bonus

--- script.js
+++ script.js
@@ -2,12 +2,78 @@ const pixelBoard = document.getElementById('pixel-board');
 const colorPalette = document.getElementById('color-palette');
 const button = document.getElementById('clear-board');
 const paletteColors = [];
-const pixelCreate = [];
+let pixelCreate = [];
 const linePixels = 5;
-const pixelDimension = linePixels * linePixels;
+//const pixelDimension = linePixels * linePixels;
 const colorNumber = 4;
+let linePixels2 = 5;
+let lastLinePixel = linePixels2;
+
+const inputValue = document.getElementById('board-size');
+const buttonInput = document.getElementById('generate-board');
+let firstBoard = true;
+
+function cleanLastBoard() {
+
+  let lastPixelsDimension = lastLinePixel * lastLinePixel;
+  for (let index = 0; index < lastPixelsDimension; index += 1) {
+    pixelBoard.removeChild(pixelCreate[index]);
+  }
+  pixelCreate = [];
+}
+
+function catchImputValue() {
+  linePixels2 = inputValue.value;
+  if (linePixels2 === '') {
+    alert('Board inválido!');
+  } else if (linePixels2 < 5) {
+    linePixels2 = 5;
+    cleanLastBoard();
+    pixelInit(linePixels2);
+    pixelEventsInit();
+  } else if (linePixels2 > 50) {
+    linePixels2 = 50;
+    cleanLastBoard();
+    pixelInit(linePixels2);
+    pixelEventsInit();
+  } else {
+    cleanLastBoard();
+    pixelInit(linePixels2);
+    pixelEventsInit();
+  }
+}
+
+buttonInput.addEventListener('click', catchImputValue);
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Iniciando a paleta de cores
+
+// console.log(Math.ceil(Math.random() * (256 - 0) + 0));
+
+function randomColor() {
+  return Math.ceil(Math.random() * (256 - 0) + 0);
+}
+
+function setRgbColor() {
+  let red = randomColor();
+  let green = randomColor();
+  let blue = randomColor();
+
+  let rgb = `rgb(${red}, ${green}, ${blue})`;
+  return rgb;
+}
+
 for (let index = 0; index < colorNumber; index += 1) {
   paletteColors[index] = document.createElement('div');
   colorPalette.appendChild(paletteColors[index]).className = 'color';
@@ -15,17 +81,29 @@ for (let index = 0; index < colorNumber; index += 1) {
 
 paletteColors[0].style.backgroundColor = 'black';
 paletteColors[0].classList.add('selected');
-paletteColors[1].style.backgroundColor = 'blue';
-paletteColors[2].style.backgroundColor = 'green';
-paletteColors[3].style.backgroundColor = 'red';
+paletteColors[1].style.backgroundColor = setRgbColor();
+paletteColors[2].style.backgroundColor = setRgbColor();
+paletteColors[3].style.backgroundColor = setRgbColor();
 
-// Iniciando o pixel board
-// Concatenado atomaticamente pelo VSCode - Problema Lint
-pixelBoard.style.width = `${(40 * linePixels) + 10}px`;
+pixelBoard.style.width = `${(40 * linePixels2) + 40}px`;
 
-for (let index = 0; index < pixelDimension; index += 1) {
+let pixelsDimension = linePixels2 * linePixels2;
+for (let index = 0; index < pixelsDimension; index += 1) {
   pixelCreate[index] = document.createElement('div');
   pixelBoard.appendChild(pixelCreate[index]).className = 'pixel';
+}
+
+// Iniciando o pixel board
+function pixelInit(pixelsNumber) {
+  // Concatenado atomaticamente pelo VSCode - Problema Lint
+  lastLinePixel = pixelsNumber;
+  pixelBoard.style.width = `${(40 * pixelsNumber) + 40}px`;
+
+  pixelsDimension = pixelsNumber * pixelsNumber;
+  for (let index = 0; index < pixelsDimension; index += 1) {
+    pixelCreate[index] = document.createElement('div');
+    pixelBoard.appendChild(pixelCreate[index]).className = 'pixel';
+  }
 }
 
 // Implementando a função de selecionar cor
@@ -57,6 +135,12 @@ function changePixelColor(value) {
 
 for (let index = 0; index < pixelCreate.length; index += 1) {
   pixelCreate[index].addEventListener('click', changePixelColor);
+}
+
+function pixelEventsInit() {
+  for (let index = 0; index < pixelCreate.length; index += 1) {
+    pixelCreate[index].addEventListener('click', changePixelColor);
+  }
 }
 
 // Implementando o botão
