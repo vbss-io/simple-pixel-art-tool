415a394b57fe90124d4330320dbd9e2e45ccae5e
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-16 16:27:05 -0500

    Corrigindo Lint

--- script.js
+++ script.js
@@ -3,74 +3,32 @@ const colorPalette = document.getElementById('color-palette');
 const button = document.getElementById('clear-board');
 const paletteColors = [];
 let pixelCreate = [];
-const linePixels = 5;
-//const pixelDimension = linePixels * linePixels;
 const colorNumber = 4;
-let linePixels2 = 5;
-let lastLinePixel = linePixels2;
+let linePixels = 5;
+let lastLinePixel = linePixels;
 
 const inputValue = document.getElementById('board-size');
 const buttonInput = document.getElementById('generate-board');
-let firstBoard = true;
 
 function cleanLastBoard() {
-
-  let lastPixelsDimension = lastLinePixel * lastLinePixel;
+  const lastPixelsDimension = lastLinePixel * lastLinePixel;
   for (let index = 0; index < lastPixelsDimension; index += 1) {
     pixelBoard.removeChild(pixelCreate[index]);
   }
   pixelCreate = [];
 }
 
-function catchImputValue() {
-  linePixels2 = inputValue.value;
-  if (linePixels2 === '') {
-    alert('Board inválido!');
-  } else if (linePixels2 < 5) {
-    linePixels2 = 5;
-    cleanLastBoard();
-    pixelInit(linePixels2);
-    pixelEventsInit();
-  } else if (linePixels2 > 50) {
-    linePixels2 = 50;
-    cleanLastBoard();
-    pixelInit(linePixels2);
-    pixelEventsInit();
-  } else {
-    cleanLastBoard();
-    pixelInit(linePixels2);
-    pixelEventsInit();
-  }
-}
-
-buttonInput.addEventListener('click', catchImputValue);
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Iniciando a paleta de cores
-
-// console.log(Math.ceil(Math.random() * (256 - 0) + 0));
-
 function randomColor() {
   return Math.ceil(Math.random() * (256 - 0) + 0);
 }
 
 function setRgbColor() {
-  let red = randomColor();
-  let green = randomColor();
-  let blue = randomColor();
+  const red = randomColor();
+  const green = randomColor();
+  const blue = randomColor();
 
-  let rgb = `rgb(${red}, ${green}, ${blue})`;
+  const rgb = `rgb(${red}, ${green}, ${blue})`;
   return rgb;
 }
 
@@ -85,9 +43,9 @@ paletteColors[1].style.backgroundColor = setRgbColor();
 paletteColors[2].style.backgroundColor = setRgbColor();
 paletteColors[3].style.backgroundColor = setRgbColor();
 
-pixelBoard.style.width = `${(40 * linePixels2) + 40}px`;
+pixelBoard.style.width = `${(40 * linePixels) + 40}px`;
 
-let pixelsDimension = linePixels2 * linePixels2;
+let pixelsDimension = linePixels * linePixels;
 for (let index = 0; index < pixelsDimension; index += 1) {
   pixelCreate[index] = document.createElement('div');
   pixelBoard.appendChild(pixelCreate[index]).className = 'pixel';
@@ -144,7 +102,6 @@ function pixelEventsInit() {
 }
 
 // Implementando o botão
-
 function clearBoard() {
   for (let index = 0; index < pixelCreate.length; index += 1) {
     pixelCreate[index].style.backgroundColor = 'white';
@@ -152,3 +109,26 @@ function clearBoard() {
 }
 
 button.addEventListener('click', clearBoard);
+
+function catchImputValue() {
+  linePixels = inputValue.value;
+  if (linePixels === '') {
+    alert('Board inválido!');
+  } else if (linePixels < 5) {
+    linePixels = 5;
+    cleanLastBoard();
+    pixelInit(linePixels);
+    pixelEventsInit();
+  } else if (linePixels > 50) {
+    linePixels = 50;
+    cleanLastBoard();
+    pixelInit(linePixels);
+    pixelEventsInit();
+  } else {
+    cleanLastBoard();
+    pixelInit(linePixels);
+    pixelEventsInit();
+  }
+}
+
+buttonInput.addEventListener('click', catchImputValue);
